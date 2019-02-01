86d421e6b0f80f987a8ef0a23be35f7a77563c4b
landiveo <landiveo@163.com>
2019-02-01 21:01:25 +0800

    change sequence to daily and weekly

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -2,114 +2,132 @@
 import json
 import numpy as np
 from datetime import datetime
+from wasserstein_distance import *
 
 class Dataset:
-    def __init__(self, data_rpath, weekday_seeds_rpath, weekend_seeds_rpath, weekday_distance_rpath, weekend_distance_rpath):
+    def __init__(self, data_rpath, daily_seeds_rpath=None, weekly_seeds_rpath=None, daily_distance_rpath=None, weekly_distance_rpath=None):
         self.data_rpath = data_rpath
-        self.weekday_seeds_rpath = weekday_seeds_rpath
-        self.weekend_seeds_rpath = weekend_seeds_rpath
-        self.weekday_distance_rpath = weekday_distance_rpath
-        self.weekend_distance_rpath = weekend_distance_rpath
+        self.daily_seeds_rpath = daily_seeds_rpath
+        self.weekly_seeds_rpath = weekly_seeds_rpath
+        self.daily_distance_rpath = daily_distance_rpath
+        self.weekly_distance_rpath = weekly_distance_rpath
+        self.load_seeds()
         self.load_data()
         self.load_distance()
 
     def get_commit_profile(self, commits):
         commit_profile = {
-            'weekdays': {},
-            'weekends': {}
+            'daily': {},
+            'weekly': {}
         }
 
-        for i in range(24):
-            commit_profile['weekdays'][i] = 0
-            commit_profile['weekends'][i] = 0
+        for i in range(24): # hour of day
+            commit_profile['daily'][i] = 0
+        for i in range(1, 8):
+            commit_profile['weekly'][i] = 0
 
         for c in commits:
-            time = datetime.strptime(c['commit']['author']['date'][:19], '%Y-%m-%dT%H:%M:%S')
-            # 工作日/周末 平均每天在每一时间段占所有工作量的比重
-            if time.isoweekday() <= 5:
-                commit_profile['weekdays'][time.hour] += 1
-            else:
-                commit_profile['weekends'][time.hour] += 1
+            time = c['commit']['author']['date'][:19]
+            time = datetime.strptime(time, '%Y-%m-%dT%H:%M:%S')
+            commit_profile['daily'][time.hour] += 1. / len(commits)
+            commit_profile['weekly'][time.isoweekday()] += 1. / len(commits)
 
-        total_commits_on_weekends = 1. * np.sum(commit_profile['weekends'].values())
-        total_commits_in_weekdays = 1. * np.sum(commit_profile['weekdays'].values())
-        if total_commits_on_weekends != 0:
-            commit_profile['weekends'] = {h: commit_profile['weekends'][h] / total_commits_on_weekends for h in commit_profile['weekends']}
-        if total_commits_in_weekdays != 0:
-            commit_profile['weekdays'] = {h: commit_profile['weekdays'][h] / total_commits_in_weekdays for h in commit_profile['weekdays']}
         return commit_profile
 
     def load_data(self):
         self.users = []
-        self.rep = {'weekdays': [], 'weekends': []}
-        self.data = {'weekdays': [], 'weekends': []}
-        self.seeds = {'weekdays': [], 'weekends': []}
-
-        #print 'Loading data from the file...'
-        with open(self.weekday_seeds_rpath, 'r') as rf:
-            weekday_seed_dict = json.load(rf)
-        self.seeds['weekdays'] = [[] for _ in range(len(set(weekday_seed_dict.values())))]
-
-        with open(self.weekend_seeds_rpath, 'r') as rf:
-            weekend_seed_dict = json.load(rf)
-        self.seeds['weekends'] = [[] for _ in range(len(set(weekend_seed_dict.values())))]
+        self.rep = {'daily': [], 'weekly': []}
+        self.data = {'daily': [], 'weekly': []}
 
         with open(self.data_rpath, 'r') as rf:
             for i, line in enumerate(rf.readlines()):
                 user = json.loads(line.strip())
 
-                if str(user['id']) in weekday_seed_dict:
-                    self.seeds['weekdays'][weekday_seed_dict[str(user['id'])]].append(i)
+                if str(user['id']) in self.daily_seed_dict:
+                    self.seeds['daily'][self.daily_seed_dict[str(user['id'])]].append(i)
 
-                if str(user['id']) in weekend_seed_dict:
-                    self.seeds['weekends'][weekend_seed_dict[str(user['id'])]].append(i)
+                if str(user['id']) in self.weekly_seed_dict:
+                    self.seeds['weekly'][self.weekly_seed_dict[str(user['id'])]].append(i)
 
                 if 'commit_profile' not in user:
                     user['commit_profile'] = self.get_commit_profile(user['commits_list'])
-                if 'weekday_rep' in user and 'weekend_rep' in user:
-                    self.rep['weekdays'].append(user['weekday_rep'])
-                    self.rep['weekends'].append(user['weekend_rep'])
-                weekends = np.array(sorted(user['commit_profile']['weekends'].items(), key=lambda x: int(x[0])))[:, -1]
-                weekdays = np.array(sorted(user['commit_profile']['weekdays'].items(), key=lambda x: int(x[0])))[:, -1]
-                self.data['weekdays'].append(weekdays.astype('float'))
-                self.data['weekends'].append(weekends.astype('float'))
+                if 'daily_rep' in user and 'weekly_rep' in user:
+                    self.rep['daily'].append(user['daily_rep'])
+                    self.rep['weekly'].append(user['weekly_rep'])
+                weekly = np.array(sorted(user['commit_profile']['weekly'].items(), key=lambda x: int(x[0])))[:, -1]
+                daily = np.array(sorted(user['commit_profile']['daily'].items(), key=lambda x: int(x[0])))[:, -1]
+                self.data['daily'].append(daily.astype('float'))
+                self.data['weekly'].append(weekly.astype('float'))
                 self.users.append(user)
 
-        self.data['weekdays'] = np.array(self.data['weekdays'])
-        self.data['weekends'] = np.array(self.data['weekends'])
-        self.rep['weekdays'] = np.array(self.rep['weekdays'])
-        self.rep['weekends'] = np.array(self.rep['weekends'])
+        self.data['daily'] = np.array(self.data['daily'])
+        self.data['weekly'] = np.array(self.data['weekly'])
+        self.rep['daily'] = np.array(self.rep['daily'])
+        self.rep['weekly'] = np.array(self.rep['weekly'])
         self.users = np.array(self.users)
 
     def load_distance(self):
-        self.distance = dict()
-        with open(self.weekday_distance_rpath, 'r') as rf:
-            self.distance['weekdays'] = np.array(json.load(rf))
-        with open(self.weekend_distance_rpath, 'r') as rf:
-            self.distance['weekends'] = np.array(json.load(rf))
+        self.distance = {'daily': [], 'weekly': []}
+        if self.daily_distance_rpath:
+            with open(self.daily_distance_rpath, 'r') as rf:
+                self.distance['daily'] = np.array(json.load(rf))
+        else:
+            print 'Cannot load distance from', self.daily_distance_rpath
 
+        if self.weekly_distance_rpath:
+            with open(self.weekly_distance_rpath, 'r') as rf:
+                self.distance['weekly'] = np.array(json.load(rf))
+        else:
+            print 'Cannot load distance from', self.weekly_distance_rpath
 
-    def merge_representation(self, weekday_rep_rpath, weekend_rep_rpath, wpath):
-        with open(weekday_rep_rpath, 'r') as rf:
-            weekday_rep = json.load(rf)
-        with open(weekend_rep_rpath, 'r') as rf:
-            weekend_rep = json.load(rf)
+    def load_seeds(self):
+        self.seeds = {'daily': [], 'weekly': []}
+
+        if self.daily_seeds_rpath:
+            with open(self.daily_seeds_rpath, 'r') as rf:
+                self.daily_seed_dict = json.load(rf)
+            self.seeds['daily'] = [[] for _ in range(len(set(self.daily_seed_dict.values())))]
+        else:
+            print 'Cannot load seeds from', self.daily_seeds_rpath
+            self.daily_seed_dict = {}
+
+        if self.weekly_seeds_rpath:
+            with open(self.weekly_seeds_rpath, 'r') as rf:
+                self.weekly_seed_dict = json.load(rf)
+            self.seeds['weekly'] = [[] for _ in range(len(set(self.weekly_seed_dict.values())))]
+        else:
+            print 'Cannot load seeds from', self.weekly_seeds_rpath
+            self.weekly_seed_dict = {}
+
+    def merge_representation(self, daily_rep_rpath, weekly_rep_rpath, wpath):
+        with open(daily_rep_rpath, 'r') as rf:
+            daily_rep = json.load(rf)
+        with open(weekly_rep_rpath, 'r') as rf:
+            weekly_rep = json.load(rf)
 
         with open(wpath, 'w') as wf:
-            for u, weekday_r, weekend_r in zip(self.users, weekday_rep, weekend_rep):
-                u['weekday_rep'] = weekday_r
-                u['weekend_rep'] = weekend_r
+            for u, daily_r, weekly_r in zip(self.users, daily_rep, weekly_rep):
+                u['daily_rep'] = daily_r
+                u['weekly_rep'] = weekly_r
                 wf.write(json.dumps(u) + '\n')
 
 if __name__ == '__main__':
-    dataset = Dataset(data_rpath='../data/balanced_samples.json',
-                      weekday_seeds_rpath='../data/weekday_seeds.json',
-                      weekend_seeds_rpath='../data/weekend_seeds.json',
-                      weekday_distance_rpath='../data/distance_weekdays',
-                      weekend_distance_rpath='../data/distance_weekends',
-                      )
-    #print dataset.data['weekdays'][0]
-    print dataset.seeds['weekdays']
-    #with open('../data/collection.json', 'w') as wf:
-    #    for u in dataset.users:
-    #        wf.write(json.dumps(u) + '\n')
+    print 'loading dataset...'
+    dataset = Dataset(data_rpath='data/balanced_all.json')
+
+    print 'Store user collection'
+    with open('../data/collection.json', 'w') as wf:
+        for user in dataset.users:
+            wf.write(json.dumps(user) + '\n')
+
+    print 'generating daily distance...'
+    ground_dist = ground_distance(24)
+    daily_distance = get_distance_matrix(dataset.data['daily'], ground_dist)
+    with open('../data/daily_distance', 'w') as wf:
+        json.dump(daily_distance.tolist(), wf)
+
+    print 'generating weekly distance...'
+    ground_dist = ground_distance(7)
+    weekly_distance = get_distance_matrix(dataset.data['weekly'], ground_dist)
+    with open('../data/weekly_distance', 'w') as wf:
+        json.dump(weekly_distance.tolist(), wf)
---
+++ utils/wasserstein_distance.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from data_processing import Dataset
+import json
+import numpy as np
+from pyemd import emd
+from tqdm import tqdm
+
+def ground_distance(dim):
+    # 00:00 - 24:00
+    # Mon. - Sun.
+    dis = np.zeros([dim, dim], float)
+    for i in range(dim):
+        for j in range(dim):
+            # 0到24指从0搬到24
+            dis[i, j] = min((j - i) % dim, (i - j) % dim)
+    return dis
+
+def get_distance_matrix(data, ground_distance):
+    wasserstein_distance = np.zeros([data.shape[0], data.shape[0]], float)
+    for i, p in tqdm(enumerate(data), total=data.shape[0]):
+        for j in range(i + 1, data.shape[0]):
+            wasserstein_distance[j, i] = wasserstein_distance[i, j] = emd(p, data[j], ground_distance)
+    return wasserstein_distance
